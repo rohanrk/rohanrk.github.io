a13dbb6a520585d901cf9e559e193d7d3082dc54
Rohan Ramakrishnan <rohanrk2009@gmail.com>
2024-01-01 22:49:14 -0500

    Modify some styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,29 +11,33 @@ interface HomeProps {
 const Home: React.FC = () => {
   return (
     <div className="home">
-      <h2>Contact me!</h2>
-      <ul className="contact">
-        <li>
-          <a href="mailto:rohanrk2009@gmail.com">Gmail</a>
-        </li>
-        <li>
-          <a href="https://github.com/rohanrk">Github</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/rohanramakrishnan/">Linkedin</a>
-        </li>
-      </ul>
+      <div className="contact">
+        <h2>Contact me!</h2>
+        <ul className="contact-list">
+          <li>
+            <a href="mailto:rohanrk2009@gmail.com">Gmail</a>
+          </li>
+          <li>
+            <a href="https://github.com/rohanrk">Github</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/rohanramakrishnan/">Linkedin</a>
+          </li>
+        </ul>
+      </div>
 
-      <h2>Prior Work</h2>
-      <p>Miscellaneous things I've written or worked on</p>
-      <ul className="projects">
-        <li>
-          <a href="http://hdl.handle.net/1853/63853">Evaluating Off Center Head Worn Displays</a>
-        </li>
-        <li>
-          <a href="https://apps.apple.com/us/app/brainy-sight-words/id1459542744">Brainy Phonics</a>
-        </li>
-      </ul>
+      <div className="projects">
+        <h2>Prior Work</h2>
+        <p>Miscellaneous things I've written or worked on</p>
+        <ul className="projects-list">
+          <li>
+            <a href="http://hdl.handle.net/1853/63853">Evaluating Off Center Head Worn Displays</a>
+          </li>
+          <li>
+            <a href="https://apps.apple.com/us/app/brainy-sight-words/id1459542744">Brainy Phonics</a>
+          </li>
+        </ul>
+      </div>
 
       <div className="wip">
         <p>TODO: Finish this website... someday</p>
