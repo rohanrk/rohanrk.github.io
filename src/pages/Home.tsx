ee5c9ceb75969e67d2a74d52f9e864fe56848b53
Rohan Ramakrishnan <rohanrk2009@gmail.com>
2024-03-02 17:51:58 -0800

    More home page formatting.
    Remember your divs kids.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,7 +11,6 @@ interface HomeProps {
 const Home: React.FC<{}> = () => {
   return (
     <div className="home">
-      <Banner text="Rohan Ramakrishnan" />
       <div className="intro">
         <h2>Who are you?</h2>
         <p>
@@ -20,33 +19,36 @@ const Home: React.FC<{}> = () => {
           polishing up this site but thanks for stopping by!
         </p>
       </div>
-      <h2>Contact me!</h2>
-      <ul className="contact">
-        <li>
-          <a href="mailto:rohanrk2009@gmail.com">Gmail</a>
-        </li>
-        <li>
-          <a href="https://github.com/rohanrk">Github</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/rohanramakrishnan/">Linkedin</a>
-        </li>
-      </ul>
-
-      <h2>Prior Work</h2>
-      <p>Miscellaneous things I've written or worked on</p>
-      <ul className="projects">
-        <li>
-          <a href="http://hdl.handle.net/1853/63853">
-            Evaluating Off Center Head Worn Displays
-          </a>
-        </li>
-        <li>
-          <a href="https://apps.apple.com/us/app/brainy-sight-words/id1459542744">
-            Brainy Phonics
-          </a>
-        </li>
-      </ul>
+      <div className="contact">
+        <h2>Contact me!</h2>
+        <ul>
+          <li>
+            <a href="mailto:rohanrk2009@gmail.com">Gmail</a>
+          </li>
+          <li>
+            <a href="https://github.com/rohanrk">Github</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/rohanramakrishnan/">Linkedin</a>
+          </li>
+        </ul>
+      </div>
+      <div className="projects">
+        <h2>Prior Work</h2>
+        <p>Miscellaneous things I've written or worked on</p>
+        <ul>
+          <li>
+            <a href="http://hdl.handle.net/1853/63853">
+              Evaluating Off Center Head Worn Displays
+            </a>
+          </li>
+          <li>
+            <a href="https://apps.apple.com/us/app/brainy-sight-words/id1459542744">
+              Brainy Phonics
+            </a>
+          </li>
+        </ul>
+      </div>
 
       <div className="wip">
         <p>TODO: Finish this website... someday</p>
