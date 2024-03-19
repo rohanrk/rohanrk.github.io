0fa9b3a34616b53c865db503f3adb3b4402d8688
Rohan Ramakrishnan <rohanrk2009@gmail.com>
2024-03-18 18:18:40 -0700

    Prep styles and fonts for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/career",
-        element: <Career />,
+        element: <Todo />,
       },
       {
         path: "/blog",
